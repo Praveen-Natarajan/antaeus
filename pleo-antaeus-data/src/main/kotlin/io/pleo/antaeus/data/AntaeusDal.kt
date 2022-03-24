417632c24556b0792b3bc46a4ac615f743abaceb
Praveen Natarajan <pravin21@live.in>
2022-03-25 01:20:17 +0530

    changes to fetch Invoices based on currency and pending status

--- pleo-antaeus-data/src/main/kotlin/io/pleo/antaeus/data/AntaeusDal.kt
+++ pleo-antaeus-data/src/main/kotlin/io/pleo/antaeus/data/AntaeusDal.kt
@@ -12,10 +12,8 @@ import io.pleo.antaeus.models.Customer
 import io.pleo.antaeus.models.Invoice
 import io.pleo.antaeus.models.InvoiceStatus
 import io.pleo.antaeus.models.Money
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.select
-import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.transactions.transaction
 
 class AntaeusDal(private val db: Database) {
@@ -79,5 +77,13 @@ class AntaeusDal(private val db: Database) {
         }
 
         return fetchCustomer(id)
+    }
+
+    fun fetchInvoices(currency: Currency): List<Invoice> {
+        return transaction(db) {
+            InvoiceTable
+                    .select { (InvoiceTable.currency.eq(currency.name)) and (InvoiceTable.status.eq(InvoiceStatus.PENDING.name)) }
+                    .map { it.toInvoice() }
+        }
     }
 }
