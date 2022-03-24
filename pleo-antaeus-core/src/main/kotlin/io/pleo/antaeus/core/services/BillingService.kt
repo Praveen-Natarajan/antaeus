1b217449a565b068b377c07d0485f4a2121a5952
Praveen Natarajan <pravin21@live.in>
2022-03-25 01:22:39 +0530

    Initial commit of charge method - fetch invoices based on currency in multithreaded way

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,9 +1,41 @@
 package io.pleo.antaeus.core.services
 
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.models.Currency
+import io.pleo.antaeus.models.Invoice
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.Executors
 
 class BillingService(
-    private val paymentProvider: PaymentProvider
+    private val paymentProvider: PaymentProvider, private val invoiceService: InvoiceService
 ) {
+
 // TODO - Add code e.g. here
+    fun charge() : List<String>{
+    // create threadPool based on the currency Size
+    val executor = Executors.newFixedThreadPool(Currency.values().size)
+    Currency.values().sorted().forEach {
+        //Now with currency wise thread, hit database and fetch the information for that particular currency
+        val date = Calendar.getInstance().time
+        val formatter = SimpleDateFormat.getDateTimeInstance() //or use getDateInstance()
+        val formatedDate = formatter.format(date)
+        println(formatedDate)
+        val workerThread = Runnable {
+            println("Executing thread fetch for $it" )
+            val output = listOf(invoiceService.fetchPendingInvoices(it))
+            for (output in output){
+                println("$output")
+            }
+            //Submit this batchfor Processing
+            //externalService.ProcessPayment(output)
+         //Submit the Output of this for Processing and handle the failure part
+        }
+        executor.execute(workerThread)
+    }
+    executor.shutdown()
+    println("Finished all threads ! Are you happy")
+           return emptyList()
+       }
 }
+
