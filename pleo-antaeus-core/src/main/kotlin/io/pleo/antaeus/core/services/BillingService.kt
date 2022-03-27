253d2412eab1318550a5f0d6b1c1d843a9943153
Praveen Natarajan <pravin21@live.in>
2022-03-28 00:35:15 +0530

    Added Kotling logging for better logging

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -2,7 +2,7 @@ package io.pleo.antaeus.core.services
 
 import io.pleo.antaeus.core.external.PaymentProvider
 import io.pleo.antaeus.models.Currency
-import io.pleo.antaeus.models.Invoice
+import mu.KotlinLogging
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.Executors
@@ -10,32 +10,27 @@ import java.util.concurrent.Executors
 class BillingService(
     private val paymentProvider: PaymentProvider, private val invoiceService: InvoiceService
 ) {
+    private val logger = KotlinLogging.logger {}
 
-// TODO - Add code e.g. here
-    fun charge() : List<String>{
+    fun charge() {
     // create threadPool based on the currency Size
     val executor = Executors.newFixedThreadPool(Currency.values().size)
     Currency.values().sorted().forEach {
         //Now with currency wise thread, hit database and fetch the information for that particular currency
-        val date = Calendar.getInstance().time
-        val formatter = SimpleDateFormat.getDateTimeInstance() //or use getDateInstance()
-        val formatedDate = formatter.format(date)
-        println(formatedDate)
         val workerThread = Runnable {
-            println("Executing thread fetch for $it" )
+            logger.info ("created thread for processing $it invoices" )
             val output = listOf(invoiceService.fetchPendingInvoices(it))
             for (output in output){
-                println("$output")
+                logger.info {"$output"}
             }
-            //Submit this batchfor Processing
-            //externalService.ProcessPayment(output)
+         // Send these messages to Kafka Topic
+        //externalService.ProcessPayment(output)
          //Submit the Output of this for Processing and handle the failure part
         }
         executor.execute(workerThread)
     }
     executor.shutdown()
     println("Finished all threads ! Are you happy")
-           return emptyList()
        }
 }
 
