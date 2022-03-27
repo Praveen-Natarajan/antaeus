a9520c7e286fa1e43703152657ecd01792db218e
Praveen Natarajan <pravin21@live.in>
2022-03-28 00:03:28 +0530

    Added kotlin dependency

--- pleo-antaeus-core/build.gradle.kts
+++ pleo-antaeus-core/build.gradle.kts
@@ -6,5 +6,6 @@ kotlinProject()
 
 dependencies {
     implementation(project(":pleo-antaeus-data"))
+    implementation("org.apache.kafka:kafka-clients:3.1.0")
     api(project(":pleo-antaeus-models"))
 }
