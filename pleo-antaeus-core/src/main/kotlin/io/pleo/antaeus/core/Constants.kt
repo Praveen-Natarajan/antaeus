27aaf59c7a9c4bab6ed0378aeaf77cd75a7c2d76
Praveen Natarajan <pravin21@live.in>
2022-03-30 01:47:31 +0530

    constants file to hanlde the constants in the app

---
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/Constants.kt
@@ -0,0 +1,7 @@
+package io.pleo.antaeus.core
+
+const val GROUP_ID = "group.id"
+const val BOOTSTRAP_SERVERS = "bootstrap.servers"
+const val PLEO_CONSUMER = "pleo-consumer"
+const val MY_PLEO_TOPIC = "my-pleo-topic"
+const val MY_RETRY_TOPIC = "my-retry-topic"
