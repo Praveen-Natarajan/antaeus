790b09749d13dfaf6619c4c9f9ae638ae434e700
Praveen Natarajan <pravin21@live.in>
2022-03-30 01:52:34 +0530

    Added retry logic and updated methods to handle failed and pending scenario

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,5 +1,7 @@
 package io.pleo.antaeus.core.services
 
+import io.pleo.antaeus.core.MY_PLEO_TOPIC
+import io.pleo.antaeus.core.MY_RETRY_TOPIC
 import io.pleo.antaeus.core.external.PaymentProvider
 import io.pleo.antaeus.models.Currency
 import io.pleo.antaeus.models.Invoice
@@ -25,7 +27,7 @@ class BillingService(
             output.forEach {
                 try {
                     logger.info ("processing message $it " )
-                    kafkaService.sendMessage(createMsgTxtFromInvoice(it),it.amount.currency.name)
+                    kafkaService.sendMessage(createMsgTxtFromInvoice(it),it.amount.currency.name, MY_PLEO_TOPIC)
                 } catch(e : Exception) {
                     logger.error { "Error sending messages to the topic for $it" }
                 }
@@ -38,7 +40,7 @@ class BillingService(
     }
 
     fun consume(){
-        chargeCard();
+        chargeInvoice();
     }
 
     private fun createMsgTxtFromInvoice(output: Invoice) :String {
@@ -47,10 +49,10 @@ class BillingService(
         return msg;
     }
 
-    fun chargeCard() {
+    fun chargeInvoice() {
         val consumer = kafkaService.createConsumer("localhost:9092")
         consumer.apply {
-            subscribe(listOf("my-pleo-topic"))
+            subscribe(listOf(MY_PLEO_TOPIC))
         }
         consumer.use {
             while (true) {
@@ -61,7 +63,7 @@ class BillingService(
                     //-Message will be in this format -> 761|77|PENDING|356.54|SEK
                     println("$msg")
                     println(msg[0]);
-                    when (chargeInvoice(msg[0].toInt())) {
+                    when (chargeInvoice(msg[0].toInt(), InvoiceStatus.PENDING)) {
                         true -> updateStatus(msg[0].toInt(), InvoiceStatus.PAID)
                         else -> processRetry(invoiceService.fetch(msg[0].toInt()), InvoiceStatus.FAILED)
 
@@ -71,21 +73,23 @@ class BillingService(
         }
     }
 
-    private fun processRetry(fetch: Invoice, status: InvoiceStatus) {
-        updateStatus(fetch.id, InvoiceStatus.FAILED)
-        //Send Msg to new Topic to process Failure status, which can be invoked Separately
+    private fun processRetry(invoice: Invoice, status: InvoiceStatus) {
+        updateStatus(invoice.id, InvoiceStatus.FAILED)
+        logger.info { "sending message for retry $invoice to topic: $MY_RETRY_TOPIC" }
+        //Send Msg to new Topic to process Failure status, which will be invoked Separately
+        kafkaService.sendMessage(createMsgTxtFromInvoice(invoice),invoice.amount.currency.name, MY_RETRY_TOPIC)
     }
 
     private fun updateStatus(id: Int, status: InvoiceStatus) {
         invoiceService.updateInvoice(id, status)
     }
 
-    fun chargeInvoice(id: Int): Boolean {
+    fun chargeInvoice(id: Int, invoiceStatus: InvoiceStatus): Boolean {
         var status = false
             try {
                 val invoice = invoiceService.fetch(id) //Fetch and check to avoid double charge
                 val customer = customerService.fetch(invoice.customerId)
-                if (invoice.status == InvoiceStatus.PENDING && customer.currency == invoice.amount.currency) {
+                if (invoice.status == invoiceStatus && customer.currency == invoice.amount.currency) {
                     status = paymentProvider.charge(invoice)
                 }
             } catch (e: Exception) {
@@ -104,5 +108,43 @@ class BillingService(
         }
     }
 
+    fun pendingtest(){
+        Currency.values().sorted().forEach {
+            val output : List<Invoice> = invoiceService.fetchFailedInvoices(it)
+            output.forEach {
+                logger.info { "Failed invoices are $it" }
+            }
+            logger.info { "Method completed" }
+        }
+    }
+
+    fun retryInvoices(){
+        logger.info { "--Retry Mechanism kicked in, processing retry messages" }
+        val consumer = kafkaService.createConsumer("localhost:9092")
+        consumer.apply {
+            subscribe(listOf(MY_RETRY_TOPIC))
+        }
+        consumer.use {
+            while (true) {
+                val records = consumer.poll(Duration.ofMillis(1000))
+                records.forEach {
+                    logger.info { "Retrying payment for invoice: $it" }
+                    var msg = it.value().toString().split("|")
+                    //-Message will be in this format -> 761|77|PENDING|356.54|SEK
+                    when (chargeInvoice(msg[0].toInt(), InvoiceStatus.FAILED)) {
+                        true -> updateStatus(msg[0].toInt(), InvoiceStatus.PAID)
+                        else -> processRetryFailure(msg[0].toInt())
+
+                    }
+                }
+            }
+        }
+    }
+
+    private fun processRetryFailure(id:Int){
+         //#TODO - send message and notify both Pleo team and the card Owner
+         // Implementation to notify the customer and the corresponding Pleo,operation executive about the Failure
+         // Decision needs to be taken on sending a consolidated email vs mail for each failure
+    }
 }
 
