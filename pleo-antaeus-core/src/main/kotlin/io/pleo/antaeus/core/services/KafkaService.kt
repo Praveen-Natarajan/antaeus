feed4897cc8111f91085590fde0ac6125bb41e4d
Praveen Natarajan <pravin21@live.in>
2022-03-30 01:51:41 +0530

    Modified sendMessage method to help,pass the topic name

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/KafkaService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/KafkaService.kt
@@ -1,5 +1,9 @@
 package io.pleo.antaeus.core.services
 
+import io.pleo.antaeus.core.BOOTSTRAP_SERVERS
+import io.pleo.antaeus.core.GROUP_ID
+import io.pleo.antaeus.core.MY_PLEO_TOPIC
+import io.pleo.antaeus.core.PLEO_CONSUMER
 import org.apache.kafka.clients.consumer.Consumer
 import org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG
 import org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG
@@ -9,18 +13,15 @@ import org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CON
 import org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.serialization.StringSerializer
-import java.time.Duration
 import java.util.*
 
 class KafkaService(broker: String) {
 
-    private val producer = createProducer("localhost:9092")
-    private val consumer = createConsumer("localhost:9092")
+    private val producer = createProducer(broker)
 
-
-     private fun createProducer(brokers: String): Producer<String, String> {
+    private fun createProducer(broker: String): Producer<String, String> {
         val props = Properties()
-        props["bootstrap.servers"] = brokers
+        props[BOOTSTRAP_SERVERS] = broker
         props[KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java.canonicalName
         props[VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java.canonicalName
         return KafkaProducer<String, String>(props)
@@ -28,15 +29,15 @@ class KafkaService(broker: String) {
 
      fun createConsumer(brokers: String): Consumer<String, String> {
         val props = Properties()
-        props["bootstrap.servers"] = brokers
+        props[BOOTSTRAP_SERVERS] = brokers
         props[KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
         props[VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
-        props["group.id"] = "pleo-consumer"
+        props[GROUP_ID] = PLEO_CONSUMER
         return KafkaConsumer<String, String>(props)
     }
 
-     fun sendMessage(msg : String, currency: String){
-          producer.send(ProducerRecord("my-pleo-topic", currency, msg))
+     fun sendMessage(msg : String, currency: String, topic:String){
+          producer.send(ProducerRecord(topic, currency, msg))
      }
 
 
