04be04317ab4f3d9c9073b70e92b1d22342f42d6
Praveen-Natarajan <pravin21@live.in>
2022-03-31 00:14:47 +0530

    code clean up, Remove unused imports

--- pleo-antaeus-app/src/main/kotlin/io/pleo/antaeus/app/AntaeusApp.kt
+++ pleo-antaeus-app/src/main/kotlin/io/pleo/antaeus/app/AntaeusApp.kt
@@ -65,7 +65,7 @@ fun main() {
     val billingService = BillingService(paymentProvider = paymentProvider, invoiceService = invoiceService,
             kafkaService = kafkaservice, customerService = customerService)
 
-    SchedulerService(invoiceService = invoiceService, billingService = billingService).scheduleTasks()
+    SchedulerService(billingService = billingService).scheduleTasks()
 
     // Create REST web service
     AntaeusRest(
