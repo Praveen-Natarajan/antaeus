84a751b1fb89068bbc3135437c923245a1fc7c95
Praveen-Natarajan <pravin21@live.in>
2022-04-01 21:16:59 +0530

    Renamed my consumer to match with invoice processing

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/Constants.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/Constants.kt
@@ -3,5 +3,5 @@ package io.pleo.antaeus.core
 const val GROUP_ID = "group.id"
 const val BOOTSTRAP_SERVERS = "bootstrap.servers"
 const val PLEO_CONSUMER = "pleo-consumer"
-const val MY_PLEO_TOPIC = "my-pleo-topic"
+const val MY_INVOICE_TOPIC = "my-invoice-topic"
 const val MY_RETRY_TOPIC = "my-retry-topic"
